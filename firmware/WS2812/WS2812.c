c6c5dc16f4b3a0c489f9e2700cc8128a1f8e11a3
vlad-plavat <vlad.plavat@student.upt.ro>
2023-11-27 19:54:38 +0200

    Library WS2812 now uses DMA

--- firmware/WS2812/WS2812.c
+++ firmware/WS2812/WS2812.c
@@ -8,22 +8,20 @@
 uint32_t LEDS[16];
 uint WS2812_sm;
 
+uint WS2812_dma;
 
 /*uint32_t ring[16]={0xffffff,0xff0000,0x00ff00,0x0000ff,0x000000,0xffff00,0xff00ff,0x00ffff,
 0xffffff,0xff0000,0x00ff00,0x0000ff,0x000000,0xffff00,0xff00ff,0x00ffff,};*/
 uint32_t ring[16]={0x0f0f0f,0,0,0,0x0f0000,0,0,0, 0x000f00,0,0,0,0x00000f};
+uint32_t WS2812_data[16];
 
-#define NUM_PIXELS 16
-
-static inline void put_pixel(uint32_t pixel_grb) {
-    pio_sm_put_blocking(WS2812_PIO, WS2812_sm, pixel_grb << 8u);
-}
 
 void WS2812_refresh(uint r){
     for (int i = 0; i < NUM_PIXELS; ++i) {
-        //put_pixel(ring[(i+time_us_32()/300000)%NUM_PIXELS]);
-        put_pixel(ring[ (i+r*16/(1024*16)) % NUM_PIXELS]);
+        WS2812_data[i] = (ring[ (i+r*16/(1024*16)) % NUM_PIXELS])<<8;
     }
+    //start DMA
+    dma_channel_transfer_from_buffer_now(WS2812_dma, WS2812_data, NUM_PIXELS);
 }
 
 void WS2812_init() {
@@ -33,39 +31,18 @@ void WS2812_init() {
         
 	ws2812_program_init(WS2812_PIO, WS2812_sm, offset, WS2812_PIN, 800000);
 
-   /*
-
     //configure ping-pong DMA
-    const uint dma_rx = dma_claim_unused_channel(true);
-    const uint dma_rx2 = dma_claim_unused_channel(true);
+    WS2812_dma = dma_claim_unused_channel(true);
 
-    dma_channel_config c = dma_channel_get_default_config(dma_rx);
+    dma_channel_config c = dma_channel_get_default_config(WS2812_dma);
     channel_config_set_transfer_data_size(&c, DMA_SIZE_32);
-    channel_config_set_dreq(&c, pio_get_dreq(pio, sm, false));
-    channel_config_set_read_increment(&c, false);
+    channel_config_set_dreq(&c, pio_get_dreq(WS2812_PIO, WS2812_sm, true));
+    channel_config_set_read_increment(&c, true);
     channel_config_set_write_increment(&c, false);
-	channel_config_set_chain_to(&c, dma_rx2);
-    dma_channel_configure(dma_rx, &c,
-                          angle, // write address
-                          &pio->rxf[sm], // read address
-                          DMA_RELOAD_VAL,
+    dma_channel_configure(WS2812_dma, &c,
+                          &WS2812_PIO->txf[WS2812_sm], // write address
+                          WS2812_data, // read address
+                          NUM_PIXELS,
                           false); // don't start yet
 
-
-    c = dma_channel_get_default_config(dma_rx2);
-    channel_config_set_transfer_data_size(&c, DMA_SIZE_32);
-    channel_config_set_dreq(&c, pio_get_dreq(pio, sm, false));
-    channel_config_set_read_increment(&c, false);
-    channel_config_set_write_increment(&c, false);
-	channel_config_set_chain_to(&c, dma_rx);
-    dma_channel_configure(dma_rx2, &c,
-                          angle, // write address
-                          &pio->rxf[sm], // read address
-                          DMA_RELOAD_VAL,
-                          false); // don't start yet
-
-    //start DMA
-    dma_start_channel_mask(1u << dma_rx);
-*/
-   
 }
--- firmware/WS2812/WS2812.h
+++ firmware/WS2812/WS2812.h
@@ -4,6 +4,7 @@
 #define WS2812_PIN 6
 #define WS2812_FREQ 800000
 #define WS2812_PIO pio0
+#define NUM_PIXELS 16
 
 void WS2812_init();
 void WS2812_refresh();
